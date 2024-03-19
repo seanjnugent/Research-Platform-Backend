2016708f5d1d27989f15b9e4bb9908c27f2a0bd8
seanjnugent <87038678+seanjnugent@users.noreply.github.com>
2024-03-19 09:23:12 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     host: 'localhost',
     port: 3306,
-    user: 'root',
-    password: 'ikagara1',
-    database: 'ids_dataplatform',
+    user: '',
+    password: '',
+    database: '',
   };
