3e84bb169706342c5aa0314f32fff72b1d25ee8a
seanjnugent <87038678+seanjnugent@users.noreply.github.com>
2024-09-10 10:28:56 +0100

    Update topicsdetails.js

--- routes/topicsdetails.js
+++ routes/topicsdetails.js
@@ -7,7 +7,7 @@ module.exports = (connection) => {
     // Retrieve the topic from the URL parameter
     const { topic } = req.params;
 
-    // Write your SQL query with the topic as a WHERE clause
+    // SQL query with the topic as a WHERE clause
     let query = `SELECT keyword, publicationName, publicationId, authors FROM ids_dataplatform.vwpublication_keyword WHERE keyword = '${topic}';`;
 
     connection.query(query, (error, results) => {
